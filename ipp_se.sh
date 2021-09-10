425911561a27a1069f35d484b91811a742ce2b30
Shaswot Dhungana <77196153+WhiteLuciferShaswot@users.noreply.github.com>
2021-09-10 05:05:43 +0000

    Update ipp_se.sh

--- ipp_se.sh
+++ ipp_se.sh
@@ -7,19 +7,19 @@
 
 #Permission_Provider
 cd
-cd mysql_for_android_v2
+cd mysql_for_android
 cd utilities
 chmod +x *
 
 
 cd
 
-cd mysql_for_android_v2
+cd mysql_for_android
 cd core
 chmod +x *
 
 cd
-cd mysql_for_android_v2
+cd mysql_for_android
 cd ui
 chmod +x *
 
@@ -27,6 +27,6 @@ chmod +x *
 #Setup_Executer
 
 cd
-cd mysql_for_android_v2
+cd mysql_for_android
 cd utilities
 ./setup.sh
