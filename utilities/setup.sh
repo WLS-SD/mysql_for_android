44f7b3ad9295f86311ecfdea9bc916dd850cbda0
WLS-SD <77196153+WLS-SD@users.noreply.github.com>
2021-09-11 13:05:18 +0545

    Update setup.sh

--- utilities/setup.sh
+++ utilities/setup.sh
@@ -38,13 +38,16 @@ printf "${GREEN}"
 
 echo "MySql Has installed SuccessFully "
 
-echo "Now You Can Start MySql using Commands Below"
+echo "Now You Can Use MySql using Commands Below"
 
 echo " 1:~ cd "
 echo " 2:~ start-mysql "
 echo "3:~ stop-mysql "
 echo "4:~ uninstall-mysql"
-echo " command 2 is used to start MySql"
+echo "5:~ update-mysql.sh"
+echo " command 2 is used to Start MySql"
 echo " Command 3 is used to Stop MySql"
-echo " Command 4 is used to uninstall MySql"
+echo " Command 4 is used to Uninstall MySql"
+echo " Command 5 is used to Update MySql"
+
 
