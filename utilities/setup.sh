b84cbf26c2c60f898c0d0d359ed72368180f1424
WLS-SD <77196153+WLS-SD@users.noreply.github.com>
2021-09-11 14:38:06 +0545

    Update setup.sh

--- utilities/setup.sh
+++ utilities/setup.sh
@@ -40,14 +40,16 @@ echo "MySql Has installed SuccessFully "
 
 echo "Now You Can Use MySql using Commands Below"
 
-echo " 1:~ cd "
+echo " 1:~ exit"
 echo " 2:~ start-mysql "
 echo "3:~ stop-mysql "
 echo "4:~ uninstall-mysql"
 echo "5:~ update-mysql.sh"
-echo " command 2 is used to Start MySql"
-echo " Command 3 is used to Stop MySql"
-echo " Command 4 is used to Uninstall MySql"
-echo " Command 5 is used to Update MySql"
+echo " Command 1 is to exit Termux App."
+echo " After Exiting Termux App Please Reopen Termux App and Follow Next Commands."
+echo " command 2 is used to Start MySql."
+echo " Command 3 is used to Stop MySql."
+echo " Command 4 is used to Uninstall MySql."
+echo " Command 5 is used to Update MySql."
 
 
