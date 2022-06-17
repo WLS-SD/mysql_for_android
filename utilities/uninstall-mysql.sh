edc9c07325a31c49d1db3276d865f1172cc7fc6f
WLS-SD <77196153+WLS-SD@users.noreply.github.com>
2022-06-17 07:43:36 +0545

    Update uninstall-mysql.sh

--- utilities/uninstall-mysql.sh
+++ utilities/uninstall-mysql.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
 cd
+cd mysql_for_android/ui
+echo "      "
+./stop-mysql-ui.sh
+cd
+
+echo "    "
+
+Green="\e[92m"
+printf "${Green}"
+
+echo " Do You Really Want to Uninstall Mysql & Its all  Dependencies ?"
+echo " {Y = Yes , N = No}"
+echo -n " (Y/N) :~ "
+read user
 
 
-rm -rf mysql_for_android 
+if [ $user = Y ]
+then
+
+GREEN="\e[92m"
+
+printf "${GREEN}"
+
+rm -rf mysql_for_android
 
 rm -rf start-mysql.sh
 
@@ -19,10 +40,65 @@ apt --purge remove figlet -y
 
 rm -rf uninstall-mysql.sh
 
-GREEN="\e[92m"
-
-printf "${GREEN}"
-
+Light_Magenta="\e[95m"
+printf "${Light_Magenta}"
 
 echo "You Have Successfully Uninstalled MariaDb/MySQL and it's all Dependencies."
 
+
+
+elif [ $user = y ]
+then
+
+GREEN="\e[92m"
+
+printf "${GREEN}"
+rm -rf mysql_for_android
+
+rm -rf start-mysql.sh
+
+rm -rf stop-mysql.sh
+
+rm -rf update-mysql.sh start-server.sh change-password-mysql.sh
+
+
+apt --purge remove mariadb -y
+
+
+apt --purge remove figlet -y
+
+rm -rf uninstall-mysql.sh
+
+Light_Magenta="\e[95m"
+printf "${Light_Magenta}"
+
+echo "You Have Successfully Uninstalled MariaDb/MySQL and it's all Dependencies."
+
+
+
+
+
+elif [ $user = N ]
+then
+Light_Magenta="\e[95m"
+printf "${Light_Magenta}"
+echo " Uninstalling Process Aborted !!! "
+
+
+elif [ $user = n ]
+then
+Light_Magenta="\e[95m"
+printf "${Light_Magenta}"
+echo " Uninstalling Process Aborted !!! "
+
+
+
+else
+
+
+Light_Magenta="\e[95m"
+printf "${Light_Magenta}"
+echo "You Can Only Choose Y/N , Y is for Yes & N is for No"
+
+
+fi
